49a0e52ae33a852dec3f751238bb088496ecd635
Long Sun <36977447+FattySun@users.noreply.github.com>
2026-09-17 15:04:17 +0800

    111

--- js/footer2.js
+++ js/footer2.js
@@ -1,8 +1,16 @@
-$(function() {
-	$('.footer-bottom .copyright .pc.wx').on('click',function () {
+$(function () {
+	$('.footer-bottom .copyright .pc.wx').on('click', function () {
 		$('.footer-bottom .pcQRcode').addClass('on')
 	})
-	$('.footer-bottom .pcQRcode').on('click',function() {
+	$('.footer-bottom .pcQRcode').on('click', function () {
+		$(this).removeClass('on')
+	})
+
+
+	$('.footer-bottom .copyright .pc.dy').on('click', function () {
+		$('.footer-bottom .pcDYQRcode').addClass('on')
+	})
+	$('.footer-bottom .pcDYQRcode').on('click', function () {
 		$(this).removeClass('on')
 	})
 })
